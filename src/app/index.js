45cf243e999a2212ee26be4eb6be401c66681a8f
Tony-404 <chernani2003@gmail.com>
2021-08-31 17:37:56 -0500

    modal imagen innerHTML

--- src/app/index.js
+++ src/app/index.js
@@ -15,3 +15,27 @@ const combrarYa = document.getElementById('combrarYa')
 combrarYa.addEventListener('click', () => {
     alert(3)
 })
+
+let items_pequeños = document.getElementById('items_pequeños')
+
+items_pequeños.innerHTML = `
+<div class="grid-item-pequeño">
+<img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630421820/Workshop%2031Agosto/Frame_16_babliz.png" alt="" >
+</div>
+<div class="grid-item-pequeño">
+<img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417970/Workshop%2031Agosto/Frame_14_y0u0da.png" alt=""  >
+</div>
+<div class="grid-item-pequeño">
+<img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417970/Workshop%2031Agosto/Frame_15_lbxmo0.png" alt=""  >
+</div>
+<div class="grid-item-pequeño">
+<img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417970/Workshop%2031Agosto/Frame_15_lbxmo0.png" alt=""  >
+</div>
+`
+
+let items_grande = document.getElementById('items_grande')
+
+items_grande.innerHTML = `
+<img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630421820/Workshop%2031Agosto/Frame_16_babliz.png" alt="">
+
+`
