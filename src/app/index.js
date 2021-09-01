981e59f5eb1327a62be6b305028a6d1e07f1a62b
Tony-404 <chernani2003@gmail.com>
2021-08-31 19:08:44 -0500

    manda dinamicamente y obsoletamente----opstento mejoras

--- src/app/index.js
+++ src/app/index.js
@@ -23,64 +23,37 @@ combrarYa.addEventListener('click', () => {
 
 function pintar(data) {
     let items_pequeños = document.getElementById('items_pequeños')
-    data.map((elementos,indice) => {
-        for(var i = 0; i <= 4; i++){
-            console.log(2)
-        }
-        items_pequeños.innerHTML = `
-        <div class="grid-item-pequeño">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630421820/Workshop%2031Agosto/Frame_16_babliz.png" alt="" >
-        </div>
-        <div class="grid-item-pequeño">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417970/Workshop%2031Agosto/Frame_14_y0u0da.png" alt=""  >
-        </div>
-        <div class="grid-item-pequeño">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417970/Workshop%2031Agosto/Frame_15_lbxmo0.png" alt=""  >
-        </div>
-        <div class="grid-item-pequeño">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417970/Workshop%2031Agosto/Frame_15_lbxmo0.png" alt=""  >
-        </div>
-        `
-    })
+    for(let i = 0; i < data[0].carrusel.length; i++){
+    items_pequeños.innerHTML += `
+    <div class="grid-item-pequeño">
+    <img src="${data[0].carrusel[i]}" alt="" >
+    </div>
+    `   
+    }
+
 // =======================================================================================
     let items_grande = document.getElementById('items_grande')
     
     items_grande.innerHTML = `
-    <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630421820/Workshop%2031Agosto/Frame_16_babliz.png" alt="">
+    <img src="${data[0].imagen}" alt="">
     `
 
 // ======================================================================================
 
     let stop_botLane = document.getElementById('stop_botLane')
+    for(let i = 0; i < data.length; i++){
+        console.log(2)
+        stop_botLane.innerHTML += `
+        <div class="grid-column-producto">
+        <div class="imagen-producto">
+            <img src="${data[i].imagen}" alt="">
+        </div>
+        <div class="descripcion-producto">
+            <p class="nombre-producto">${data[i].nombre}</p>
+            <p class="precio-producto">$${data[i].precio}</p>
+        </div>
+        </div>
+        `
+    }
 
-    stop_botLane.innerHTML = `
-    <div class="grid-column-producto">
-    <div class="imagen-producto">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417957/Workshop%2031Agosto/Rectangle_1_rkjpy2.png" alt="">
-    </div>
-    <div class="descripcion-producto">
-        <p class="nombre-producto">Nombre producto</p>
-        <p class="precio-producto">$0000</p>
-    </div>
-    </div>
-    <div class="grid-column-producto">
-    <div class="imagen-producto">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417955/Workshop%2031Agosto/Frame_20_ind8rr.png" alt="">
-    </div>
-    <div class="descripcion-producto">
-        <p class="nombre-producto">Nombre producto</p>
-        <p class="precio-producto">$0000</p>
-    </div>
-
-    </div>
-    <div class="grid-column-producto">
-    <div class="imagen-producto">
-        <img src="https://res.cloudinary.com/dobboq5dt/image/upload/v1630417958/Workshop%2031Agosto/Frame_17_rpc5yn.png" alt="">
-    </div>
-    <div class="descripcion-producto">
-        <p class="nombre-producto">Nombre producto</p>
-        <p class="precio-producto">$0000</p>
-    </div>
-    </div>
-    `
 }
