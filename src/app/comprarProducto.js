c2b398f3871b58302f5c9e2c34dc8a6c1f199703
Tony-404 <chernani2003@gmail.com>
2021-08-31 23:35:44 -0500

    muestra carril, manda al local storage, recive,  error al seleccionar

--- src/app/comprarProducto.js
+++ src/app/comprarProducto.js
@@ -8,3 +8,55 @@
 
 //     localStorage.setItem('compraDirecto', JSON.stringify(elementos))
 // }
+
+
+
+// function pintar(data) {
+//     let items_pequeños = document.getElementById('items_pequeños')
+//     for(let i = 0; i < data[0].carrusel.length; i++){
+//     items_pequeños.innerHTML += `
+//     <div class="grid-item-pequeño">
+//     <img src="${data[0].carrusel[i]}" alt="" >
+//     </div>
+//     `   
+//     }
+
+// // =======================================================================================
+//     let items_grande = document.getElementById('items_grande')
+    
+//     items_grande.innerHTML = `
+//     <img src="${data[0].imagen}" alt="">
+//     `
+
+// // ======================================================================================
+
+//     let stop_botLane = document.getElementById('stop_botLane')
+//     for(let i = 0; i < data.length; i++){
+//         stop_botLane.innerHTML += `
+//         <div class="grid-column-producto">
+//         <div class="imagen-producto">
+//             <img src="${data[i].imagen}" alt="">
+//         </div>
+//         <div class="descripcion-producto">
+//             <p class="nombre-producto">${data[i].nombre}</p>
+//             <p class="precio-producto">$${data[i].precio}</p>
+//         </div>
+//         </div>
+//         `
+//     }
+
+//     let botones = document.getElementById('botones')
+
+//     botones.innerHTML=`
+//     <div>
+//     <button class="btn1" id="añadirCarrito">ADD TO CART</button>
+//     </div>
+//     <div>
+//     <div style="display:none" class="comprarYaDiv">
+//     <p class="nombre-producto">${data[0].nombre}</p>
+//     <h1 class="precio-producto">$${data[0].precio}</h1>
+//     </div>
+//     <button class="btn2 comprarYa">BUY  IT NOW</button>
+//     </div>
+//     `
+// }
--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,16 @@ const url = 'http://localhost:4000/producto'
 // import conseguirId from './comprarProducto'
 // console.log(url)
 document.addEventListener('click', e => {
+    pintarBarra(e)
     conseguirId(e)
     // pintar()
 })
 
 let carritoComprarYa = []
-
-fetch(url).then((v) => v.json()).then((d) => {pintar(d)})
+document.addEventListener('DOMContentLoaded', () =>
+{
+    fetch(url).then((v) => v.json()).then((d) => {pintarProductos(d)})
+})
 
 
 // const añadirAlCarrito = document.getElementById('añadirCarrito')
@@ -23,56 +26,73 @@ fetch(url).then((v) => v.json()).then((d) => {pintar(d)})
 //     alert(3)
 // })
 
-function pintar(data) {
-    let items_pequeños = document.getElementById('items_pequeños')
-    for(let i = 0; i < data[0].carrusel.length; i++){
-    items_pequeños.innerHTML += `
-    <div class="grid-item-pequeño">
-    <img src="${data[0].carrusel[i]}" alt="" >
-    </div>
-    `   
-    }
-
-// =======================================================================================
-    let items_grande = document.getElementById('items_grande')
-    
-    items_grande.innerHTML = `
-    <img src="${data[0].imagen}" alt="">
-    `
-
-// ======================================================================================
-
+function pintarProductos(data){
     let stop_botLane = document.getElementById('stop_botLane')
     for(let i = 0; i < data.length; i++){
+        stop_botLane.classList.add('mostrar')
         stop_botLane.innerHTML += `
-        <div class="grid-column-producto">
-        <div class="imagen-producto">
-            <img src="${data[i].imagen}" alt="">
-        </div>
-        <div class="descripcion-producto">
-            <p class="nombre-producto">${data[i].nombre}</p>
-            <p class="precio-producto">$${data[i].precio}</p>
-        </div>
-        </div>
+                <div class="imagen-producto">
+                <img src="${data[i].imagen}" style="width:300px; height:400px;" alt="">
+                    <div class="productos">
+                    <h5 class="nombre-producto">${data[i].nombre}</h5>
+                    <p class="precio-producto">$${data[i].precio}</p>
+                    <div style="display:none">
+                        <h3>Prueba</h3>
+                        <img class="itemI1" src="${data[i].carrusel[0]}">
+                        <img class="itemI2" src="${data[i].carrusel[1]}">
+                        <img class="itemI3" src="${data[i].carrusel[2]}">
+                        <img class="itemI4" src="${data[i].carrusel[3]}">
+                    </div>
+                    </div>
+                    </div>
         `
     }
-
-    let botones = document.getElementById('botones')
-
-    botones.innerHTML=`
-    <div>
-    <button class="btn1" id="añadirCarrito">ADD TO CART</button>
-    </div>
-    <div>
-    <div style="display:none" class="comprarYaDiv">
-    <p class="nombre-producto">${data[0].nombre}</p>
-    <h1 class="precio-producto">$${data[0].precio}</h1>
-    </div>
-    <button class="btn2 comprarYa">BUY  IT NOW</button>
-    </div>
-    `
 }
 
+
+
+const pintarBarra = e => {
+    if(e.target.classList.contains('productos')){
+        pintarLocal(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const pintarLocal = object => {
+    const elemento = {
+        nombre: object.querySelector('h5').textContent,
+        precio: object.querySelector('p').textContent,
+        imagenTitle: object.querySelector('img').src,
+        img1: object.querySelector('.itemI1').src,
+        img2: object.querySelector('.itemI2').src,
+        img3: object.querySelector('.itemI3').src,
+        img4: object.querySelector('.itemI4').src
+    }
+    localStorage.setItem('dio', JSON.stringify(elemento))
+    localStorage.setItem('porfin', JSON.stringify(elemento.img2))
+    pintarBarraLocal(elemento)
+}
+
+function pintarBarraLocal(elemento){
+    let items_pequeños = document.getElementById('items_pequeños')
+    items_pequeños.innerHTML =  `
+    <div class="grid-item-pequeño">
+    <img src="${elemento.img1}" alt="" >
+    </div>
+    <div class="grid-item-pequeño">
+    <img src="${elemento.img2}" alt="" >
+    </div>
+    <div class="grid-item-pequeño">
+    <img src="${elemento.img3}" alt="" >
+    </div>
+    <div class="grid-item-pequeño">
+    <img src="${elemento.img4}" alt="" >
+    </div>
+    `   
+
+}
+
+// =========================================================================
 function conseguirId(e){
     if(e.target.classList.contains('comprarYa')){
         comprarYa(e.target.parentElement)
